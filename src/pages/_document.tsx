95cb127427c72b86c375673a2c64e04331a6fd9c
achrafsekri <achrafsekri2001@gmail.com>
2024-05-07 11:34:26 +0100

    Update dependencies in package-lock.json and package.json

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,12 @@
-import { Head, Html, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from "next/document";
+import { Analytics } from "@vercel/analytics/react";
 
 function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <Analytics />
         <div id="skip-navigation" />
         <Main />
         <NextScript />
